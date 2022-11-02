7975182d7377b38a828a903a52c6230f0baa3319
marcodiazz <marcopadel21@gmail.com>
2022-11-03 00:17:25 +0100

    marcador

--- renderer.js
+++ renderer.js
@@ -5,10 +5,18 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
- function returnText(){
-    let jugador1 = document.getElementById("jugador1").value;
+let jugador1;
+
+function setPlayers(){
+  document.getElementById("nombre1").innerHTML = "Marco";
+  console.log(jugador1);
+}
+
+  function returnText(){
+    jugador1 = document.getElementById("jugador1").value;
     let jugador2 = document.getElementById("jugador2").value;
     let jugador3 = document.getElementById("jugador3").value;
     let jugador4 = document.getElementById("jugador4").value;
     window.location = "marcador.html";
+    setPlayers(jugador1);
   }
